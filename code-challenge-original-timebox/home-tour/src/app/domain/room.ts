b278682250aafc60bf0dd8653e80dd2a6aa5c628
Newteq Developer <github@newteq.co.za>
2024-05-13 01:53:01 +0200

    Add original completed code, because history was lost a little on this

---
+++ code-challenge-original-timebox/home-tour/src/app/domain/room.ts
@@ -0,0 +1,25 @@
+export class Room {
+  readonly roomImageRoot = 'assets/house';
+  readonly floorPlanImageRoot = 'assets/floorplan';
+
+  name: string = '';
+  description: string = '';
+
+  private floorPlanImage: string = '';
+  private images: string[] = [];
+
+  getImageUrls(): string[] {
+    return this.images.map(x => `${this.roomImageRoot}/${x}`);
+  }
+
+  getFloorPlanImage(): string {
+    return `${this.floorPlanImageRoot}/${this.floorPlanImage}`;
+  }
+
+  constructor(name: string, description: string, images: string[], floorPlanImage: string) {
+    this.name = name;
+    this.description = description;
+    this.images = images;
+    this.floorPlanImage = floorPlanImage;
+  }
+}
---
+++ code-challenge-original-timebox/home-tour/src/app/domain/variables/image-details.ts
@@ -0,0 +1,23 @@
+export class ImageDetails {
+  static readonly imageRoot = 'assets/house/';
+
+  static readonly imageDetails =
+    [
+      'living01.jpg',
+      'living02.jpg',
+      'living03.jpg',
+      'living04.jpg',
+      'bedroom01.jpg',
+      'bedroom02.jpg',
+      'bedroom03.jpg',
+      'bedroom04.jpg',
+      'balcony01.jpg',
+      'bathroom01.jpg',
+      'bathroom02.jpg',
+      'hallway01.jpg',
+      'kitchen01.jpg',
+      'kitchen02.jpg',
+      'outside01.jpg',
+      'outside02.jpg'
+    ]
+}
