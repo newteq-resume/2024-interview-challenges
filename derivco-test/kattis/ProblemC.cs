d43c227d0c31a05c876ef45362b8c59d2066a448
Newteq Developer <github@newteq.co.za>
2024-06-10 00:57:43 +0200

    Get started with C

---
+++ derivco-test/kattis/ProblemC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis
+{
+    /*
+     * this problem can use the BFS again with different rules
+     * 
+     * see, for this one, we get the starting point.
+     * this will either be 1 or 0, so either decimal or binary (respectively)
+     * if we have move along all the 1's or 0's depending on what we are, to the end, then we can move.
+     * otherwise, it will simply be "neither" as the output
+     */
+    
+    internal class ProblemC : IProblem
+    {
+        public void Run()
+        {
+            var testData = @"10 20
+11111111111111111111
+11000000000000000101
+11111111111111110000
+11111111111111110000
+11000000000000000111
+00011111111111111111
+00111111111111111111
+10000000000000001111
+11111111111111111111
+11111111111111111111
+3
+2 3 8 16
+8 1 7 3
+1 1 10 20
+";
+
+
+        }
+    }
+}
