a44633476f2174e0641ce73eebc9802b3b90a500
Newteq Developer <github@newteq.co.za>
2024-06-09 16:26:24 +0200

    Get data into grid

--- derivco-test/kattis/ProblemB.cs
+++ derivco-test/kattis/ProblemB.cs
@@ -10,7 +10,40 @@ namespace kattis
     {
         public void Run()
         {
-            throw new NotImplementedException();
+            var inputParams = Console.ReadLine().Split(' ');
+            var t = Convert.ToInt32(inputParams[0]);
+            var rows = Convert.ToInt32(inputParams[1]);
+            var cols = Convert.ToInt32(inputParams[2]);
+
+            var grid = new char[rows, cols];
+
+            var lines = new List<string>();
+            for(var i = 0; i < rows; i++)
+            {
+                lines.Add(Console.ReadLine());
+            }
+
+            var currentRow = 0;
+            var currentCol = 0;
+            foreach (var line in lines)
+            {
+                foreach (var item in line)
+                {
+                    grid[currentRow, currentCol] = item;
+                    currentCol++;
+                }
+                currentRow++;
+                currentCol = 0;
+            }
+
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < cols; j++)
+                {
+                    Console.Write(grid[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
