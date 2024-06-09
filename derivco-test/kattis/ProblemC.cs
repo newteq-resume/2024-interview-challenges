3a8af18327cda46d253212c8d550ec3d5f962cf5
Newteq Developer <github@newteq.co.za>
2024-06-10 01:49:03 +0200

    Commiting for reference
    Check out how queueItem is referenced

--- derivco-test/kattis/ProblemC.cs
+++ derivco-test/kattis/ProblemC.cs
@@ -36,140 +36,133 @@ namespace kattis
 8 1 7 3
 1 1 10 20
 ";
-
+            // for testing
             using var reader = new StringReader(testData);
             Console.SetIn(reader);
-            var a = Console.ReadLine();
-            Console.WriteLine(a);
-            //    var inputParams1 = Console.ReadLine().Split(' ');
-            //    var rows = Convert.ToInt32(inputParams1[0]);
-            //    var cols = Convert.ToInt32(inputParams1[1]);
 
-            //    var inputMap = new List<string>();
-            //    for(int i = 0; i < rows; i++)
-            //    {
-            //        inputMap.Add(Console.ReadLine());
-            //    }
-            //    var gridMap = new char[rows, cols];
-            //    // the above gives us the map that we need to look at
+            var inputParams1 = Console.ReadLine().Split(' ');
+            var rows = Convert.ToInt32(inputParams1[0]);
+            var cols = Convert.ToInt32(inputParams1[1]);
 
-            //    var inputNumberOfMoves = Convert.ToInt32(Console.ReadLine());
-            //    var inputMoves = new List<((int,int), (int, int))>();
-            //    for (int i = 0; i < inputNumberOfMoves; i++)
-            //    {
-            //        var line = Console.ReadLine().Split(' ');
-            //        var numbersInLine = new List<int>();
-            //        foreach(var item in line)
-            //        {
-            //            numbersInLine.Add(Convert.ToInt32(item));
-            //        }
-            //        var startMove = (numbersInLine[0], numbersInLine[1]);
-            //        var endMove = (numbersInLine[2], numbersInLine[3]);
-            //        inputMoves.Add((startMove, endMove));
-            //    }
-            //    // now - inputMoves are all the moves that we need to go through
+            var inputMap = new List<string>();
+            for (int i = 0; i < rows; i++)
+            {
+                inputMap.Add(Console.ReadLine());
+            }
+            var gridMap = new char[rows, cols];
+            // the above gives us the map that we need to look at
 
-            //    var currentRow = 0;
-            //    var currentCol = 0;
-            //    Tuple<int, int> startingPos = null;
-            //    Tuple<int, int> endingPos = null;
-            //    foreach (var line in inputMap)
-            //    {
-            //        foreach (var item in line)
-            //        {
-            //            gridMap[currentRow, currentCol] = item;
-            //            currentCol++;
-            //        }
-            //        currentRow++;
-            //        currentCol = 0;
-            //    }
+            var inputNumberOfMoves = Convert.ToInt32(Console.ReadLine());
+            var inputMoves = new List<((int, int), (int, int))>();
+            for (int i = 0; i < inputNumberOfMoves; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var numbersInLine = new List<int>();
+                foreach (var item in line)
+                {
+                    numbersInLine.Add(Convert.ToInt32(item));
+                }
+                var startMove = (numbersInLine[0], numbersInLine[1]);
+                var endMove = (numbersInLine[2], numbersInLine[3]);
+                inputMoves.Add((startMove, endMove));
+            }
+            // now - inputMoves are all the moves that we need to go through
 
-            //    var (canReadEnd, numberOfMovesToEnd) = CanReachTarget(grid, (startingPos.Item1, startingPos.Item2), maxNumberOfMoves);
-            //    if (!canReadEnd)
-            //        Console.WriteLine("NOT POSSIBLE");
-            //    else
-            //        Console.WriteLine(numberOfMovesToEnd);
+            var currentRow = 0;
+            var currentCol = 0;
+            foreach (var line in inputMap)
+            {
+                foreach (var item in line)
+                {
+                    gridMap[currentRow, currentCol] = item;
+                    currentCol++;
+                }
+                currentRow++;
+                currentCol = 0;
+            }
 
-            //}
+            foreach (var (startingPos, endingPos) in inputMoves)
+            {
+                // we're going to use the canReachEnd var to do some short curcuiting where applicable
+                var (canReachEnd, who) = CanReachTarget(gridMap, startingPos, endingPos);
+                if (!canReachEnd)
+                    Console.WriteLine("neither");
+                else
+                    Console.WriteLine(who);
+            }
 
-            //// https://www.geeksforgeeks.org/breadth-first-traversal-bfs-on-a-2d-array/
-            //public (bool, int) CanReachEnd(char[,] grid, (int x, int y) startingPos, int maxMoves)
-            //{
-            //    var directions = new (char, int, int)[]
-            //    {
-            //        ('D', -1, 0), // up - row above, col same
-            //        ('U', 1, 0),  // down - row below, col same
-            //        ('R', 0, -1), // left - left col, row same
-            //        ('L', 0, 1)   // right - right col, row same
-            //    };
+        }
 
-            //    var maxRows = grid.GetLength(0);
-            //    var maxCols = grid.GetLength(1);
+        public (bool, string) CanReachTarget(char[,] gridMap, (int x, int y) startingPos, (int x, int y) endingPos)
+        {
+            var directions = new (int, int)[]
+            {
+                    (-1, 0), // up - row above, col same
+                    (1, 0),  // down - row below, col same
+                    (0, -1), // left - left col, row same
+                    (0, 1)   // right - right col, row same
+            };
 
-            //    var visited = new bool[maxRows, maxCols];
+            var whoAreWe = gridMap[startingPos.x, startingPos.y] == '1' ? "decimal" : "binary";
+            var allowedPerson = gridMap[startingPos.x, startingPos.y];
+            // we're not moving, so short curcuit
+            if (startingPos.x == endingPos.x && startingPos.y == endingPos.y)
+            {
+                return (true, whoAreWe);
+            }
 
-            //    // Stores indices of the matrix cells
-            //    var queue = new Queue<QueueMovement>();
+            var maxRows = gridMap.GetLength(0);
+            var maxCols = gridMap.GetLength(1);
 
-            //    // Mark the starting cell as visited and push it into the queue
-            //    queue.Enqueue(new QueueMovement
-            //    {
-            //        XPos = startingPos.x,
-            //        YPos = startingPos.y,
-            //        Moves = 0
-            //    });
-            //    visited[startingPos.x, startingPos.y] = true;
+            var visited = new bool[maxRows, maxCols];
 
-            //    // Iterate while the queue is not empty
-            //    while (queue.Count != 0)
-            //    {
-            //        var queueItem = queue.Dequeue();
+            // Stores indices of the matrix cells
+            var queue = new Queue<(int x, int y)>();
 
-            //        // check all surrounding cells
-            //        foreach (var (allowedDirection, dirX, dirY) in directions)
-            //        {
-            //            var nextX = queueItem.XPos + dirX;
-            //            var nextY = queueItem.YPos + dirY;
-            //            var nextMoveCount = queueItem.Moves + 1;
-            //            if (IsValid(grid, visited, allowedDirection, nextX, nextY, maxRows, maxCols))
-            //            {
-            //                if (IsBorder(nextX, nextY, maxRows, maxCols) && nextMoveCount <= maxMoves)
-            //                {
-            //                    return (true, nextMoveCount);
-            //                }
-            //                queue.Enqueue(new QueueMovement
-            //                {
-            //                    XPos = nextX,
-            //                    YPos = nextY,
-            //                    Moves = nextMoveCount
-            //                });
+            // Mark the starting cell as visited and push it into the queue
+            queue.Enqueue((startingPos.x, startingPos.y));
+            visited[startingPos.x, startingPos.y] = true;
 
-            //                visited[nextX, nextY] = true;
-            //            }
-            //        }
-            //    }
+            // Iterate while the queue is not empty
+            while (queue.Count != 0)
+            {
+                var queueItem = queue.Dequeue();
 
-            //    return (false, -1);
-            //}
+                // check all surrounding cells
+                foreach (var (dirX, dirY) in directions)
+                {
+                    var nextX = queueItem.x + dirX;
+                    var nextY = queueItem.y + dirY;
+                    if (IsValid(gridMap, visited, allowedPerson, nextX, nextY, maxRows, maxCols))
+                    {
+                        if ()
+                        {
+                            return (true, whoAreWe);
+                        }
+                        queue.Enqueue((nextX, nextY));
+                        visited[nextX, nextY] = true;
+                    }
+                }
+            }
 
-            //public bool IsValid(char[,] grid, bool[,] visited, char allowedDirection, int nextX, int nextY, int maxX, int maxY)
-            //{
-            //    if (nextX < 0 || nextX >= maxX || nextY < 0 || nextY >= maxY)
-            //        return false;
-            //    if (visited[nextX, nextY])
-            //        return false;
-            //    if (grid[nextX, nextY] == '0')
-            //        return true;
-            //    if (grid[nextX, nextY] == allowedDirection)
-            //        return true;
+            return (false, string.Empty);
+        }
 
-            //    return false;
-            //}
+        public bool IsValid(char[,] grid, bool[,] visited, char allowedPerson, int nextX, int nextY, int maxX, int maxY)
+        {
+            if (nextX < 0 || nextX >= maxX || nextY < 0 || nextY >= maxY)
+                return false;
+            if (visited[nextX, nextY])
+                return false;
+            if (grid[nextX, nextY] == allowedPerson)
+                return true;
 
-            //public bool IsBorder(int x, int y, int maxRows, int maxCols)
-            //{
-            //    return x == 0 || x == maxRows - 1 || y == 0 || y == maxCols - 1;
-            //}
+            return false;
+        }
+
+        public bool IsEnd(int xEnd, int yEnd)
+        {
+            return  == 0 || x == maxRows - 1 || y == 0 || y == maxCols - 1;
         }
     }
 }
